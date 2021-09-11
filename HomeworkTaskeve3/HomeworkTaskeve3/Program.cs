c4a56cd6143d7216f97d4f2e62cdad911563bb61
elnurabbaszade <elnursha@code.edu.az>
2021-09-11 12:53:50 +0400

    Add files via upload

---
+++ HomeworkTaskeve3/HomeworkTaskeve3/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+namespace HomeworkTaskeve3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) verilmish ededin tek ve ya cut eded oldugunu yoxla
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a%2==0)
+            //{
+            //    Console.WriteLine("eded cutdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededtekdir");
+            //}
+
+            //2) 2 eded verilib. Eger I eded II edede tam bolunurse o zaman cavabi cixart. 
+            //Eks halda  "bolunmur" sozunu cap et
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //if (a % b == 0)
+            //{
+            //    Console.WriteLine(a/b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bolunmur");
+            //}
+
+            // 3) Verilmish ededdin sonuncu reqemi 7 ile qurtarirmi? Beli ve Xeyr cavabini yaz ekrana
+
+            //l1:
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //if (a % 10==7)
+            //{
+            //    Console.WriteLine("beli") ;
+            //    goto l1;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("xeyir");
+            //    goto l1;
+            //} 
+
+            //4) Verilmihs 2 reqemli ededin reqemleri eynidirmi?   Beli ve Xeyr cavabini yaz ekrana
+            //l1:
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //if (a%11==0)
+            //{
+            //    Console.WriteLine("eded eynidir");
+            //    goto l1;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded eyni deyil");
+            //    goto l1;
+            //}
+
+            //5) Verilmish 2 reqemli ededdin reqemleri cemi cut ededdirse o zaman
+
+            //hemin 2 reqemli ededdin reqemlerinin hasilini ekrana cixart.
+
+            //l1:
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //    int b = a % 10;
+            //    int c = a / 10;
+            //    int d = b + c;
+            //    if (a>10&& a<100)
+            //    {
+            //        if (d%2==0)
+            //        {
+            //            Console.WriteLine(b * c);
+            //            goto l1;
+            //        }
+
+
+            //6) Verilmish 3 reqemli ededdin I ve III reqemlerinin cemi II reqeme beraberdirmi?  Beli ve Xeyr cavabini yaz ekrana
+
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = a % 10;
+
+            //int c = a % 100/10;
+
+            //int k = a % 1000/100;
+
+            //if (c == b + k)
+            //{
+            //    Console.WriteLine("Beli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("xeyir");
+            //}
+            //Console.Read();
+
+
+
+
+
+            //  7) Verilmish 3 reqemli ededdin reqemleri cemi I reqemin kvadratina beraberdirmi?  Beli ve Xeyr cavabini yaz ekrana
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = a %10;
+            //int c = a % 100 / 10;
+            //int d = a % 1000 / 100;
+
+            //if (b+c+d==(d*d))
+            //{
+            //    Console.WriteLine("beli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Xeyir");
+            //}
+
+
+            //8) SWITCH operatoru ile yoxla. Verilmish ededdin sonuncu reqemi 1 olarsa - "I gun" sozunu
+            //2 olarsa - "II gun" sozunu... 7 olarsa - "VI gun" sozunu yaz 
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //int b = a % 10;
+
+            //switch (b)
+            //{
+
+            //    case 1:
+
+            //        {
+            //            Console.WriteLine("I gun");
+            //            break;
+            //        }
+            //    case 2:
+            //        {
+            //            Console.WriteLine("II gun");
+            //            break;
+            //        }
+            //    case 3:
+            //        {
+            //            Console.WriteLine("III gun");
+            //            break;
+            //        }
+
+            //    case 4:
+
+            //        {
+            //            Console.WriteLine("IV gun");
+            //            break;
+            //        }
+            //    case 5:
+            //        {
+            //            Console.WriteLine("V gun");
+            //            break;
+            //        }
+            //    case 6:
+            //        {
+            //            Console.WriteLine("VI gun");
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            Console.WriteLine("teyin edilmeyib");
+            //            break;
+            //        }
+            //       
+
+            //}
+
+
+            //9) SWITCH operatoru ile yoxla. 
+            //   Verilmish ededdin 1 - ile 12 arasinda olugunu yoxla.(1 ve 12 ozude daxil olmaq sherti ile)
+            //   Eger bu eded 1 olarsa - "Yanvar" sozunu 2 olarsa - "Fevral" sozunu...12 olarsa - "Dekabr" sozunu yaz
+
+            //Sual
+
+
+
+
+            //10) IF operatoru ile yoxla.  6 dene eded verilib.Bu ededlerden hansi en boyukdur.
+
+            //int boyuk = 0;
+            //int kicik = 0;
+            //int a;
+            //for (int i = 1; i < =10; i++)
+            //{
+            //    Console.Write("eded daaxil et ",i);
+            //}
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (i==1)
+            //{
+            //    boyuk = a;
+            //    kicik = a;
+            //}
+            //Sual     
+
+
+
+
+
+
+
+
+
+            //11) Verilmish 6 reqemli ededin ilk 3 reqeminin cemi son 3 reqeminin cemine beraberdirse
+            //    o zaman  bu ededdin ilk 2 reqemini son 2 reqemine yapishdir.
+            // Neticenin ustune ortaya dushen 2 reqemini gel. 
+
+
+
+
+
+            //12) Verilmihs 6 reqemli ededdin evveline ve axirina 8 artir.// Alinan neticenin ilk 4 reqemi ile son 4 reqeminin yerini deyish
+            //Neticenin tek yerde dayanan reqemlerini 0 reqemi ile evez et.
+
+
+
+
+
+
+            //13) Verilmish 7 reqemli eded polindromdursa o zaman onun daxilnde nece dene 0 reqemi oldugunu tap ?
+
+
+
+            //14) Bir eded daxil et. Bu eded maksimum 8 reqemli ola biler(bu o demekdirki 1, 2, 3, 4, 5, 6, 7 reqemlide ola biler)
+            // Bu ededin evvelin 1 reqemini artir.
+
+            //int a = 1234567;
+            //int b = a % 10000000+10000000 ;
+            //Console.WriteLine(b);
+
+
+            //15) IF operatoru ile yoxla. Verilmish ededdin 1 - ile 365 arasinda olugunu yoxla(1 ve 365 ozude daxil olmaq sherti ile)
+            // Ededin ilin hansi ayina dushduyunu tap. Ve hemin ay ve gune uygun hansi burc oldugunu goster
+
+
+
+
+
+            //16)*Verilmish 6 reqemli ededde nece unikal reqem var ? (UNIKAL - Ededin daxilde her reqemden bir numayende secilecek. Yekunda nece numayende olacaq?)
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
